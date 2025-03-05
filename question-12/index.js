d6a4757cc6031351ee363dba4e1e79ca33058b65
anjanshrestha8 <anjanshrestha2023@gmail.com>
2025-03-05 23:26:29 +0545

    fix for question 7

--- question-12/index.js
+++ question-12/index.js
@@ -1,16 +1,14 @@
-let array = [1, 2, 3, 4, 5, 6];
+let array = [1, -2, 3, -4, 5, 6];
+let result = [];
 let sum = 0;
 
-function findTotalSum(data) {
-  for (let i = 0; i < data.length; i++) {
-    sum += data[i];
+function checkIfPositive(data) {
+  for (let i = 0; i <= data.length; i++) {
+    if (data[i] > 0) {
+      result.push(data[i]);
+    }
   }
-  return sum;
+  return result;
 }
 
-function average(data) {
-  let result = findTotalSum(data);
-  return result / data.length;
-}
-
-console.log(average(array));
+console.log(checkIfPositive(array));
--- question-7/index.js
+++ question-7/index.js
@@ -1,7 +1,7 @@
-let sum = 10;
+let sum = 0;
 for (let i = 10; i <= 30; i++) {
   if (i % 2 === 0) {
-    sum += 10;
+    sum += i;
   }
 }
 console.log(sum);
